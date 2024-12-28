4f4929b5928ae6e6b9792f65b027b93c2f466425
Anton Mazur <toxas99@icloud.com>
2024-12-29 00:23:48 +0200

    Finish hw <Single Responsibility>

---
+++ Classes/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Classes;
+
+class Product {
+    private $name;
+    private $value;
+
+    public function getName() {}
+
+    public function setName($name) {}
+
+    public function getValue() {}
+
+    public function setValue($value) {}
+}
---
+++ Classes/ProductRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Classes;
+
+class ProductRepository {
+    public function save(Product $product) {}
+
+    public function update(Product $product) {}
+
+    public function delete(Product $product) {}
+}
